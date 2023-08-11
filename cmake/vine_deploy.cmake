08efa052d1344997ee8ade1396425e06249de1fd
jiuzhou <yong-zh@qq.com>
2023-08-11 16:30:05 +0800

    sync

--- cmake/vine_deploy.cmake
+++ cmake/vine_deploy.cmake
@@ -2,10 +2,11 @@ set(VINE_DEPLOY_TARGET Deploy)
 add_custom_target(${VINE_DEPLOY_TARGET} ALL)
 get_target_property(WINDEPLOYQT_EXE Qt6::windeployqt IMPORTED_LOCATION)
 message(windeployqt:${WINDEPLOYQT_EXE})
+message(--------VCINSTALLDIR:${VCINSTALLDIR})
 if(WIN32)
     add_custom_command (
         TARGET ${VINE_DEPLOY_TARGET}
         POST_BUILD
-        COMMAND ${CMAKE_COMMAND} -E env VCINSTALLDIR="D:/Program Files/Microsoft Visual Studio/2022/Enterprise/" ${WINDEPLOYQT_EXE} $<TARGET_FILE:vi::AppfwGui>
+        COMMAND ${WINDEPLOYQT_EXE} --verbose 2  $<TARGET_FILE:vi::AppfwGui>
         COMMENT "Run Qt6::windeployqt")
 endif()
--- include/graphics/Geometry.h
+++ include/graphics/Geometry.h
@@ -5,7 +5,7 @@
 VINE_GRAPHICS_NS_BEGIN
 
 class VINE_GRAPHICS_API Geometry : public Inherit<Object, Geometry>{
-
+    
 };
 
 VINE_GRAPHICS_NS_END
