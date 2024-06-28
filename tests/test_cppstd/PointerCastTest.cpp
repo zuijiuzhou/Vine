8616674b8a065809f6be6a840287544a51a38adc
jiuzhou <yong-zh@qq.com>
2024-06-28 15:40:14 +0800

    --

---
+++ tests/test_cppstd/PointerCastTest.cpp
@@ -0,0 +1,33 @@
+#include <gtest/gtest.h>
+#include <core/String.h>
+
+class Base1 {
+public:
+    void function() {
+        std::cout << "Base1 function" << std::endl;
+    }
+};
+
+class Base2 {
+public:
+    void function() {
+        std::cout << "Base2 function" << std::endl;
+    }
+};
+
+class Derived : public Base1, public Base2 {
+public:
+    void function() {
+        std::cout << "Derived function" << std::endl;
+    }
+};
+
+TEST(PointerCast, upCast)
+{
+    Derived d;
+    Base1* b1 = (Base1*)&d;
+    Base2* b2 = (Base2*)&d;
+    std::cout << b1 << std::endl;
+    std::cout << b2 << std::endl;
+    ASSERT_EQ((void*)b1, b2);
+}
