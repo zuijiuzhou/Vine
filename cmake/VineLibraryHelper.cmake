c30c436a7ec8ca554ea75467b49537c5decf99ba
zuijiuzhou <yong-zh@qq.com>
2025-12-15 22:18:35 +0800

    --

--- cmake/VineLibraryHelper.cmake
+++ cmake/VineLibraryHelper.cmake
@@ -1,72 +1,72 @@
-function(vi_add_library VAR_TARGET_NAME SHORT_NAME)
+function(vi_add_library target_name_var short_name)
     # CMAKE_SOURCE_DIR顶级当前CMakeLists.txt文件所在目录
     # CMAKE_CURRENT_SOURCE_DIR 当前CMakeLists.txt所在目录
-    file(RELATIVE_PATH SOURCE_REL_DIR ${CMAKE_SOURCE_DIR}/src ${CMAKE_CURRENT_SOURCE_DIR})
+    file(RELATIVE_PATH src_rel_dir ${CMAKE_SOURCE_DIR}/src ${CMAKE_CURRENT_SOURCE_DIR})
     # SDK头文件所在目录
-    set(SDK_INCLUDE_DIR ${CMAKE_SOURCE_DIR}/include/${SOURCE_REL_DIR})
+    set(sdk_inc_dir ${CMAKE_SOURCE_DIR}/include/${src_rel_dir})
     # SDK头文件
-    file(GLOB SDK_HEADER_FILE_LIST ${SDK_INCLUDE_DIR}/*.hpp ${SDK_INCLUDE_DIR}/*.h)
+    file(GLOB sdk_header_file_list ${sdk_inc_dir}/*.hpp ${sdk_inc_dir}/*.h)
     # CPP文件
-    file(GLOB SRC_FILE_LIST LIST_DIRECTORIES false *.cpp)
+    file(GLOB src_file_list LIST_DIRECTORIES false *.cpp)
     # 头文件
-    file(GLOB HEADER_FILE_LIST LIST_DIRECTORIES false *.hpp *.h)
+    file(GLOB header_file_list LIST_DIRECTORIES false *.hpp *.h)
     # 资源文件
-    file(GLOB RC_FILE_LIST LIST_DIRECTORIES false *.rc)
+    file(GLOB rc_file_list LIST_DIRECTORIES false *.rc *.qrc)
     # 目标名称
-    set(TARGET_NAME ${SHORT_NAME})
+    set(target_name ${short_name})
     # 目标别名
-    set(TARGET_ALIAS ${VI_SHARED_LIBRARY_PREFIX}::${SHORT_NAME})
+    set(target_alias ${VI_SHARED_LIBRARY_PREFIX}::${short_name})
     # 库文件名称
-    set(LIB_FILE_NAME $<$<PLATFORM_ID:Linux>:lib>${VI_SHARED_LIBRARY_PREFIX}${SHORT_NAME})
+    set(lib_file_name $<$<PLATFORM_ID:Linux>:lib>${VI_SHARED_LIBRARY_PREFIX}${short_name})
 
     # 创建目标
-    add_library(${TARGET_NAME} SHARED ${SDK_HEADER_FILE_LIST} ${HEADER_FILE_LIST} ${SRC_FILE_LIST} ${RC_FILE_LIST})
+    add_library(${target_name} SHARED ${sdk_header_file_list} ${header_file_list} ${src_file_list} ${rc_file_list})
     # 设置目标别名
-    add_library(${TARGET_ALIAS} ALIAS ${TARGET_NAME})
+    add_library(${target_alias} ALIAS ${target_name})
     # 设置输出文件名
-    set_target_properties(${TARGET_NAME} PROPERTIES OUTPUT_NAME ${LIB_FILE_NAME})
+    set_target_properties(${target_name} PROPERTIES OUTPUT_NAME ${lib_file_name})
     # 设置输出分组
-    set_target_properties(${TARGET_NAME} PROPERTIES FOLDER vi)
+    set_target_properties(${target_name} PROPERTIES FOLDER vi)
 
-    set_target_properties(${TARGET_NAME} PROPERTIES PREFIX "")
+    set_target_properties(${target_name} PROPERTIES PREFIX "")
     # 设置源文件分组
-    source_group(sdk FILES ${SDK_HEADER_FILE_LIST})
-    # source_group(inc FILES ${HEADER_FILE_LIST})
-    # source_group(src FILES ${SRC_FILE_LIST})
-    # source_group(res FILES ${RC_FILE_LIST})
+    source_group(sdk FILES ${sdk_header_file_list})
+    # source_group(inc FILES ${header_file_list})
+    # source_group(src FILES ${src_file_list})
+    # source_group(res FILES ${rc_file_list})
 
-    string(TOUPPER VI_${SHORT_NAME}_LIB LIB_COMPILE_DEF)
-    target_compile_definitions(${TARGET_NAME} PRIVATE ${LIB_COMPILE_DEF})
+    string(TOUPPER VI_${short_name}_LIB lib_compile_def)
+    target_compile_definitions(${target_name} PRIVATE ${lib_compile_def})
 
-    if("${VAR_TARGET_NAME}" STREQUAL "")
+    if("${target_name_var}" STREQUAL "")
         message(SEND_ERROR "输出的目标名称变量名不能为空")
     else()
-        set(${VAR_TARGET_NAME} ${TARGET_NAME} PARENT_SCOPE)
+        set(${target_name_var} ${target_name} PARENT_SCOPE)
     endif()
 
     # PRIVATE   仅作用与目标自身
     # PUBLIC    同时作用于目标自身与依赖该目标的目标
     # INTERFACE 仅作用于依赖该目标的目标
-    target_include_directories(${TARGET_NAME}
+    target_include_directories(${target_name}
         PUBLIC
         "$<BUILD_INTERFACE:${CMAKE_SOURCE_DIR}/include>"
         "$<INSTALL_INTERFACE:include>")
 
-    # string(TOLOWER ${SHORT_NAME} ShortName_lowercase)
+    # string(TOLOWER ${short_name} ShortName_lowercase)
     # string(TOLOWER ${CMAKE_PROJECT_NAME} proj_name_lowercase)
 
     # 安装头文件
-    install(FILES ${SDK_HEADER_FILE_LIST} DESTINATION ${CMAKE_INSTALL_PREFIX}/include/${SOURCE_REL_DIR})
+    install(FILES ${sdk_header_file_list} DESTINATION ${CMAKE_INSTALL_PREFIX}/include/${src_rel_dir})
     # 安装目标文件
     install(
-        TARGETS ${TARGET_NAME}
+        TARGETS ${target_name}
         EXPORT ${CMAKE_PROJECT_NAME}Targets
         RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR}
         LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR}
         ARCHIVE DESTINATION ${CMAKE_INSTALL_LIBDIR})
 
-        
 
-    message(--------AddLib:${TARGET_ALIAS})
+
+    message(--------AddLib:${target_alias})
 
 endfunction()
--- src/vine/graphics/Drawable.cpp
+++ src/vine/graphics/Drawable.cpp
@@ -6,6 +6,8 @@ VI_OBJECT_META_IMPL(Drawable, RefObject)
 
 struct Drawable::Data {};
 
-Drawable::Drawable() : d(new Data()) {}
+Drawable::Drawable()
+  : d(new Data())
+{}
 
 VI_GRAPHICS_NS_END
